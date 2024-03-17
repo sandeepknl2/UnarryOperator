67fa903d4b4ec0b319f72405c123ea67edc229db
sandeepknl2 <161841885+sandeepknl2@users.noreply.github.com>
2024-03-17 11:47:07 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,14 @@
+class Unary {
+	public static void main(String[] args){
+	int x=50;
+	int y=x++;
+	System.out.println(x+" "+y);
+
+	System.out.println("-----------");
+
+
+	int a=23;
+	int b=--a;
+	System.out.println(a+" "+b);
+	}
+}
